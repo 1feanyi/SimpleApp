493524f822fdcdc690909fe76c9e84758febbc2e
1feanyi <ifeanyi.igbo@gmail.com>
2015-05-13 09:46:13 -0400

    Init commit on experimental project
    Created main project to vote on color and display system info.
    Demonstrating module that self registers to replace timer class in main
    app...

---
+++ SimpleApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Mvc;
+using SimpleApp.Models;
+using System.Collections.Generic;
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(GetTimeStamps());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Color color)
+        {
+            Color? oldColor = Session["color"] as Color?;
+
+            if (oldColor != null)
+            {
+                Votes.ChangeVote(color, (Color)oldColor);
+            }
+            else
+            {
+                Votes.RecordVote(color);
+            }
+            ViewBag.SelectedColor = Session["color"] = color;
+            return View(GetTimeStamps());
+        }
+
+        private List<string> GetTimeStamps()
+        {
+            return new List<string>
+            {
+                string.Format("App timestamp: {0}", HttpContext.Application["app_timestamp"]),
+                string.Format("Request timestamp: {0}", Session["request_timestamp"])
+            };
+        }
+    }
+}
---
+++ SimpleApp/Infrastructure/TimerModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+
+namespace SimpleApp.Infrastructure
+{
+    public class TimerModule : IHttpModule
+    {
+        private Stopwatch timer;
+
+        public void Init(HttpApplication app) //registers the HandleEvent method as a handler for the Begin/End events (event handler)
+        {
+            app.BeginRequest += HandleEvent;
+            app.EndRequest += HandleEvent;
+        }
+
+        private void HandleEvent(object src, EventArgs args)
+        {
+            HttpContext ctx = HttpContext.Current;
+            if (ctx.CurrentNotification == RequestNotification.BeginRequest) // cue to start timer at application_start
+            {
+                timer = Stopwatch.StartNew();
+            }
+            else // triggered when the EndRequest event occurs
+            {
+                ctx.Response.Write(string.Format("<div class = 'alert alert-success'>Elapsed: {0:F5} seconds</div>", ((float)timer.ElapsedTicks) / Stopwatch.Frequency));
+            }
+        }
+
+        public void Dispose()
+        {
+            // do nothing - no resources to release
+        }
+    }
+}
