80ce0b2de85ef1c77db471ca5c2b316cd37065f3
iffy <ifeanyiigbo@chemicalmarketing.com>
2015-05-13 15:16:37 -0400

    Created the ModuleRegistration class

--- CommonModules/InfoModule.cs
+++ CommonModules/InfoModule.cs
@@ -9,7 +9,7 @@ namespace CommonModules
             app.EndRequest += (src, args) =>
             {
                 HttpContext ctx = HttpContext.Current;
-                ctx.Response.Write(string.Format("<div>URL: {0} Status {1}</div>", ctx.Request.RawUrl, ctx.Response.StatusCode));
+                ctx.Response.Write(string.Format("<div class='alert alert-success'>URL: {0} Status {1}</div>", ctx.Request.RawUrl, ctx.Response.StatusCode));
             };
         }
         public void Dispose()
