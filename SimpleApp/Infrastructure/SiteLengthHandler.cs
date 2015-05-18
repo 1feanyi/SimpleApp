89c0307f7dac2365fc2dbba69c6d2aa183e80f86
iffy <ifeanyiigbo@chemicalmarketing.com>
2015-05-18 16:43:41 -0400

    few handler and factory updates

---
+++ SimpleApp/Infrastructure/SiteLengthHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SimpleApp.Infrastructure
+{
+    public class SiteLengthHandler : HttpTaskAsyncHandler
+    {
+        public override async Task ProcessRequestAsync(HttpContext context) // async method
+        {
+            string data = await new HttpClient().GetStringAsync("http://www.google.com"); // reaches out to the destination may be a site, file, db, api, etc
+            context.Response.ContentType = "text/html";
+            context.Response.Write(string.Format("<span>Length: {0}</span>", data.Length));
+        }
+    }
+}
